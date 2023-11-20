a6d769d1848702a6217842856140fd42baad4364
su8y <su8y@Baesua>
2023-11-20 14:59:00 +0900

    Gauge 재고수량 테스트

--- src/main/java/com/hae/demo/DemoApplication.java
+++ src/main/java/com/hae/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(OrderConfigV0.class)
+@Import({ OrderConfigV0.class, StockConfigV1.class })
 public class DemoApplication {
 
     public static void main(String[] args) {
