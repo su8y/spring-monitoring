14db5fea927b7c29dd9bc8b6710fcf35b589853b
su8y <su8y@Baesua>
2023-11-20 13:16:18 +0900

    보안과 매트릭

--- src/main/java/com/hae/demo/DemoApplication.java
+++ src/main/java/com/hae/demo/DemoApplication.java
@@ -1,13 +1,20 @@
 package com.hae.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
     }
 
 }
